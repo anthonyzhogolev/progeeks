ee7ee209fa7922c88c793a4f4dd2000dcfb300ac
Anton Zhogolev <ale7496@gmail.com>
2017-10-27 19:51:29 +0500

    updates

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,69 +1,73 @@
-function SumbitContactForm()
-{
-    jQuery('#ajax-contact').submit();
+// function SumbitContactForm() {
+//     jQuery('#ajax-contact').submit();
+// }
+
+$("#ajax-contact").validate({
+    debug: true,
+    rules: {
+        name: {required: true},
+        email: {required: true},
+        message: {required: true},
+    },
+    messages: {
+        name: "Name is required",
+        email: "Email is required",
+        message: "Message id required",
+    },
+    submitHandler: function (form) {
+        grecaptcha.execute();
+    },
+
+    onkeyup: false
+});
+
+function showLoader($show){
+    if($show){
+        $('#contact-form-loader').show();
+        $('#contact-form-btn').hide();
+    } else {
+        $('#contact-form-loader').hide();
+        $('#contact-form-btn').show();
+    }
 }
 
-$(function () {
-
-    // Get the form.
+function SumbitContactForm() {
     var form = $('#ajax-contact');
 
+    var formData = {
+        'name': form.find('[name="name"]').val(),
+        'email': form.find('[name="email"]').val(),
+        'message': form.find('[name="message"]').val(),
+    };
+    showLoader(true);
 
-    // Get the messages div.
-    var formMessages = $('#form-messages');
-
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-
-        // Serialize the form data.
-        // var formData = $(form).serializeArray();
-        var formData = {
-            'name': form.find('[name="name"]').val(),
-            'email': form.find('[name="email"]').val(),
-            'message': form.find('[name="message"]').val(),
-        };
-        // Submit the form using AJAX.
-        $.ajax({
-            type: 'POST',
-            // url: $(form).attr('action'),
-            url: 'https://nso6cxsh4f.execute-api.us-east-1.amazonaws.com/progeeks/send-message',
-            data: JSON.stringify(formData),
-            dataType: 'json',
-            contentType: 'application/json',
-        })
-            .done(function (response) {
-                // Make sure that the formMessages div has the 'success' class.
-                // $(formMessages).removeClass('error');
-                // $(formMessages).addClass('success');
-
-                // Set the message text.
-                // $(formMessages).text(response);
-
-                $('#centralModalSuccess').modal('show');
-
-                // Clear the form.
-                $('#name').val('');
-                $('#email').val('');
-                $('#message').val('');
-            })
-            .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    // $(formMessages).text(data.responseText);
-                    $('#centralModalWarning').modal('show');
-                } else {
-                    // $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                    // toastr.info('Oops! An error occured and your message could not be sent.');
-                    $('#centralModalWarning').modal('show');
-                }
-            });
-
+    $.ajax({
+        type: 'POST',
+        // url: $(form).attr('action'),
+        url: 'https://nso6cxsh4f.execute-api.us-east-1.amazonaws.com/progeeks/send-message',
+        data: JSON.stringify(formData),
+        dataType: 'json',
+        contentType: 'application/json',
+    }).done(function (response) {
+        showLoader(false);
+        $('#centralModalSuccess').modal('show');
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    }).fail(function (data) {
+        showLoader(false);
+        var $modal = $('#centralModalWarning');
+        $modal.find('.js-modal-response-message').hide();
+        $modal.find('.js-modal-default-message').hide();
+        // Set the message text.
+        if (data.responseText !== '') {
+            $modal.find('.js-modal-response-message').text(responseText);
+            $modal.find('.js-modal-response-message').show();
+            $modal.modal('show');
+        } else {
+            $modal.find('.js-modal-default-message').show();
+            $modal.modal('show');
+        }
     });
-
-});
+}
